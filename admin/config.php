e4147d9dfb95d3c38b703f3c31f60de8ae985486
fvahid <vahidfardi2011@gmail.com>
2016-01-02 12:55:13 +0330

    after connect to database

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://84.241.41.22:8080/kalaghkala/admin/');
+define('HTTP_CATALOG', 'http://84.241.41.22:8080/kalaghkala/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://84.241.41.22:8080/kalaghkala/admin/');
+define('HTTPS_CATALOG', 'http://84.241.41.22:8080/kalaghkala/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/wamp/www/kalaghkala/admin/');
+define('DIR_SYSTEM', 'E:/wamp/www/kalaghkala/system/');
+define('DIR_LANGUAGE', 'E:/wamp/www/kalaghkala/admin/language/');
+define('DIR_TEMPLATE', 'E:/wamp/www/kalaghkala/admin/view/template/');
+define('DIR_CONFIG', 'E:/wamp/www/kalaghkala/system/config/');
+define('DIR_IMAGE', 'E:/wamp/www/kalaghkala/image/');
+define('DIR_CACHE', 'E:/wamp/www/kalaghkala/system/cache/');
+define('DIR_DOWNLOAD', 'E:/wamp/www/kalaghkala/system/download/');
+define('DIR_UPLOAD', 'E:/wamp/www/kalaghkala/system/upload/');
+define('DIR_LOGS', 'E:/wamp/www/kalaghkala/system/logs/');
+define('DIR_MODIFICATION', 'E:/wamp/www/kalaghkala/system/modification/');
+define('DIR_CATALOG', 'E:/wamp/www/kalaghkala/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'vahid');
+define('DB_DATABASE', 'kalaghkala');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
