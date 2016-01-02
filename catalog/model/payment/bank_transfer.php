49597c77e7066ded1dd7718dbfd4f55a7b98c493
fvahid <vahidfardi2011@gmail.com>
2016-01-02 14:48:15 +0330

    revert to orginal file

--- catalog/model/payment/bank_transfer.php
+++ catalog/model/payment/bank_transfer.php
@@ -1,13 +1,13 @@
 <?php
-class ModelPaymentEnbank extends Model {
+class ModelPaymentBankTransfer extends Model {
 	public function getMethod($address, $total) {
-		$this->load->language('payment/enbank');
+		$this->load->language('payment/bank_transfer');
 
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('enbank_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('bank_transfer_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
-		if ($this->config->get('enbank_total') > 0 && $this->config->get('enbank_total') > $total) {
+		if ($this->config->get('bank_transfer_total') > 0 && $this->config->get('bank_transfer_total') > $total) {
 			$status = false;
-		} elseif (!$this->config->get('enbank_geo_zone_id')) {
+		} elseif (!$this->config->get('bank_transfer_geo_zone_id')) {
 			$status = true;
 		} elseif ($query->num_rows) {
 			$status = true;
@@ -19,10 +19,10 @@ class ModelPaymentEnbank extends Model {
 
 		if ($status) {
 			$method_data = array(
-				'code'       => 'enbank',
+				'code'       => 'bank_transfer',
 				'title'      => $this->language->get('text_title'),
 				'terms'      => '',
-				'sort_order' => $this->config->get('enbank_sort_order')
+				'sort_order' => $this->config->get('bank_transfer_sort_order')
 			);
 		}
 
